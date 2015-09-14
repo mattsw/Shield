9b375d020f92d0e2bf5ca845ccec6b899b9c9959
Matt Wilhelm <mattwilhelm@cet.com>
2015-09-14 15:05:55 -0700

    Initial Project Commit

---
+++ Shield/app/shieldApp.js
@@ -0,0 +1,20 @@
+﻿(function(angular) {
+    angular.
+        module('shieldApp', ['ui.router']).
+        config(config);
+
+    config.$inject = ['$locationProvider', '$stateProvider', '$urlRouterProvider'];
+
+    function config($locationProvider, $stateProvider, $urlRouterProvider) {
+
+        $urlRouterProvider.otherwise('start');
+        $locationProvider.html5Mode(true);
+
+        $stateProvider.
+            state('start', {
+                url: '/start',
+                controller: 'startCtrl as vm',
+                templateUrl: 'app/start/start.html'
+            });
+    }
+})(window.angular);
---
+++ Shield/app/start/start.js
@@ -0,0 +1,15 @@
+﻿(function(angular) {
+    angular.
+        module('shieldApp').
+        controller('startCtrl', StartCtrl);
+
+    StartCtrl.$inject = [];
+
+    function StartCtrl() {
+        var vm = this;
+
+        this.helloText = "Try it out today!";
+
+        return vm;
+    }
+})(window.angular);
